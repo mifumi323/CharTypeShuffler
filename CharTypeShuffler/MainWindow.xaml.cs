d369b08b7ce8f49e608feb7eae9943b65923688a
mifumi323 <mifumi323@gmail.com>
2022-06-06 23:48:12 +0900

    初期コード（半角英字の大文字小文字入れ替えるだけ）

---
+++ CharTypeShuffler/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace CharTypeShuffler
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myTextBox.Text = new string(myTextBox.Text.Select(c =>
+            {
+                if ('a' <= c && c <= 'z')
+                {
+                    return (char)(c - 'a' + 'A');
+                }
+                if ('A' <= c && c <= 'Z')
+                {
+                    return (char)(c - 'A' + 'a');
+                }
+                return c;
+            }).ToArray());
+        }
+    }
+}
